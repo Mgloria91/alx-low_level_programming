87126368bf6dfda407dbeac400e14c042b3ba0ba
Mgloria91 <gloriaoisebe@gmail.com>
2023-07-14 02:40:01 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * Main - A programm that prints a line with computer type
+ * main - A programm that prints a line with computer type
  *
- * Return: Always 0 (Success)
+ * Return: Always 0 (success)
  */
 int main(void)
 {
