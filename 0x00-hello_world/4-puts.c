0ff3eb5af11cb301b93d61255ba22845df45a6fb
Mgloria91 <gloriaoisebe@gmail.com>
2023-07-14 02:13:42 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,17 +5,11 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void)
-{
-  puts("Programming is like building a multilingual puzzle\n");
-  return (0);
-}
-
-=======
+#include <stdio.h>
 
 int main(void)
 {
   puts("\"Programming is like building a multilingual puzzle");
   return (0);
 }
->>>>>>> 1102c9b85360c81ec1225eced11cb6ec3728f029
+
